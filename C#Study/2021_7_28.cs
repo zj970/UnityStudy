24cb6528fc9e86a659429e3880ca5a6aca50b326
zj970 <3060529292@qq.com>
2021-07-28 13:43:19 +0800

    2021/7/28Unity学习

---
+++ C#Study/2021_7_28.cs
@@ -0,0 +1,11 @@
+//Unity3D控制物体移动
+void Update(){
+	//控制移动
+	float h = Input.GetAxisRaw("Horizontal");
+	float v = Input.GetAxisRaw("Vertical");
+	//前提是由刚体
+	//也可以人为设置时间间隔速度
+	rigidbody.MovePosition(transform.posion + new Vector(h,0,v) * speed * Time.deltaTime);
+}
+
+
