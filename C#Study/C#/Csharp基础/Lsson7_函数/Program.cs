18167f4251c107269388389ac62c7f922e48df4c
zj970 <3060529292@qq.com>
2021-08-03 09:57:31 +0800

    基础--数组 练习题

--- C#Study/C#/Csharp基础/Lsson7_函数/Program.cs
+++ C#Study/C#/Csharp基础/Lsson7_函数/Program.cs
@@ -121,6 +121,52 @@ namespace Lsson7_函数
             }
 
             Console.WriteLine("test-----------------------------------------------");
+
+            int[] a = { 1, 2, 3, 4, 5, 6, 7 };
+            Console.WriteLine("数组的总和为：{0}\n最大值为:{1}\n最小值为:{2}\n平均值为:{3}",Compute(a)[0], Compute(a)[1], Compute(a)[2], Compute(a)[3]);
+        }
+        const float PI = 3.1415926f;
+
+        /// <summary>
+        /// 比较两个数字的大小，返回最大值
+        /// </summary>
+        /// <param name="a">浮点数</param>
+        /// <param name="b">浮点数</param>
+        /// <returns></returns>
+        static float Compare(float a, float b)
+        {
+            return a > b ? a : b;
+        }
+
+        /// <summary>
+        /// 返回浮点数组，第一位是圆的面积。第二位是圆的周长
+        /// </summary>
+        /// <param name="r">圆的半径</param>
+        /// <returns></returns>
+        static float[] Compute(float r)
+        {
+            return new float[] {PI*r*r,2*r*PI};
+        } 
+
+        /// <summary>
+        /// 计算数组的总和，最大值，最小值，平均值，并返回
+        /// </summary>
+        /// <param name="array">传入数组</param>
+        /// <returns></returns>
+        static float[] Compute(int[] array)
+        {
+            int sum = 0;
+            float avg = 0;
+            int min = array[1];
+            int max = array[1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+                max = max < array[i] ? array[i] : max;
+                min = min < array[i] ? min : array[i];
+            }
+            avg = sum / array.Length;
+            return new float[] { sum, max, min, avg };
         }
     }
 }
