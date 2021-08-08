216435bb76bae34baa8cdc3cbf8dc716958d0d27
zj970 <3060529292@qq.com>
2021-08-08 17:52:50 +0800

    测试类提交---增加

---
+++ C#Study/C#/Csharp基础/Lsson8_ref和out/Tesr.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lsson8_ref和out
+{
+    class Tesr
+    {
+         static void Test()
+        {
+            Console.Write("用于测试");
+        }
+    }
+}
