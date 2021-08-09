58a9699b0a437896ede65cc4d1f52e942ac0b0e3
zj970 <3060529292@qq.com>
2021-08-09 17:13:23 +0800

    C#进阶知识点结束

---
+++ C#Study/C#/Csharp进阶/Lesson24_值和引用/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace Lesson24_值和引用
+{
+    class Test
+    {
+
+    }
+
+    struct TestStruct
+    {
+        public Test t;
+        public int i;
+    }
+
+    #region 知识回顾
+    //值类型
+    //无符号：byte,ushort,uint,ulong
+    //有符号: sbyte,short,int,long
+    //浮点数：float,double.decrimal
+    //特殊: char,bool
+    //枚举: enum
+    //结构体:struct
+
+    //引用类型
+    //string
+    //数组
+    //class
+    //interface
+    //委托
+
+    //值类型和引用类型的本质区别
+    //值的具体内容存在栈内存上
+    //引用的具体内容存在堆内存上
+    #endregion
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lesson24_值和引用");
+
+            #region 问题一 如何判断 值类型和引用类型
+            //F12 进到类型的内部去查看
+            //是class就是引用
+            //是struct就是值
+            #endregion
+
+            #region 问题二 语句块
+            //命名空间
+            //    ⬇
+            //类、接口、结构体
+            //    ⬇
+            //函数、属性、索引器、运算符重载等（类、接口、结构体）
+            //    ⬇
+            //条件分支、循环
+
+            //上层语句块：类、结构体
+            //中层语句块：函数
+            //底层的语句块：条件分支 循环等
+
+            //我们的代码逻辑写在哪里？
+            //函数、条件分支、循环-中层底层语句块中
+
+            //我们的变量可以声明在哪里
+            //上、中、底部能声明变量
+            //上层语句块中：成员变量
+            //中、底层语句块中：临时变量
+            #endregion
+
+            #region 问题三 变量的生命周期
+            //变量的生命周期
+            //在中底层声明的临时变量（函数、条件分支、循环语句块等）
+            //语句块执行结束
+            //没有被记录的对象将被回收或变成垃圾
+            //值类型：被系统自动回收
+            //引用类型:栈上用于存地址的房间被系统自动回收，堆中具体内容变成垃圾，待下次GC回收
+
+            //想要不被回收或者不变垃圾
+            //必须将其记录下来
+            //如何记录？
+            //在更高层级记录 或者 使用静态全局变量记录
+
+            #endregion
+
+            #region 问题四 结构体中的值和引用
+            //结构体本身就是值类型
+            //前提：该结构体没有作为其他类的成员
+            //在结构体中的值，栈中存储值具体的内容
+            //在结构体中的引用，堆中存储引用具体的内容
+
+            //引用类型始终存储在堆中
+            //真正通过结构体使用其中引用类型时，只是顺藤摸瓜
+            TestStruct ts = new TestStruct();
+            ts.t = new Test();
+            #endregion
+
+            #region 问题五 类中的值和引用
+            //类本身是引用类型
+            //在类中的值，堆中存储具体的值
+            //在类中的引用，堆中存储具体的值
+
+            //值类型跟着大哥走，引用类型一根筋
+            #endregion
+
+            #region 问题六 数组中的存储规则
+            //数组本身就是引用类型
+            //值类型数组，堆中房间存在具体内容
+            //引用类型数组，堆中房间存地址
+            #endregion
+
+            #region 问题七 结构体继承接口
+            //利用里氏替换原则，用接口容器装载结构体存在装箱拆箱
+            TestS obj = new TestS();
+            obj.Value = 1;
+            Console.WriteLine(obj.Value);
+            TestS obj2 = obj;
+            obj.Value = 2;
+            Console.WriteLine(obj.Value);
+            Console.WriteLine(obj2.Value);
+
+            ITest iobj1 = obj;//装箱 value = 1
+            ITest iobj2 = iobj1;//装箱
+            iobj2.Value = 999;
+            Console.WriteLine(iobj2.Value);
+            Console.WriteLine(iobj1.Value);
+
+            TestS obj3 = (TestS)iobj1;//拆箱
+            
+            #endregion
+        }
+    }
+
+    interface ITest
+    {
+        int Value
+        {
+            get;
+            set;
+        }
+    }
+    struct TestS : ITest
+    {
+        private int val;
+        public int Value
+        {
+            get
+            {
+                return val;
+            }
+            set
+            {
+                val = value;
+            }
+        }
+    }
+}
---
+++ C#Study/C#/Csharp进阶/lesson23_特殊语法/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson23_特殊语法
+{
+    class Person
+    {
+        private int money;
+        public bool sex;
+
+        public string Name
+        {
+            get=>"吱吱吱吱";
+            set=>sex = true;
+        }
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        public int Add(int x, int y) => x + y;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lesson23_特殊语法");
+
+            #region 知识点一 var隐式类型
+            //var是一种特殊的变量类型
+            //它可以用来表示任意类型的变量
+            //注意:
+            //1.var不能作为类的成员 只能用于临时变量声明 也就是 一般写在函数语句块中
+            //2.var必须初始化
+
+            var i = 5;
+            var s = "123";
+            var array = new int[] { 1,2,3,4,5};
+            var list = new List<int>();
+            #endregion
+
+            #region 知识点二 设置对象初始值
+            //声明对象时
+            //可以通过直接写大括号的形式初始化公共成员变量和属性
+
+            Person person = new Person { sex = true, Age = 18, Name = "jk" };
+            var person1 = new Person { sex = true, Age = 18, Name = "jk" };
+            #endregion
+
+            #region 知识点三 设置集合初始值
+            //声明集合对象时
+            //也可以通过大括号 直接初始化内部属性
+            int[] array2 = new int[] { 1, 3, 54, 6, 7 };
+
+            List<int> listInt = new List<int>() { 1, 2, 5, 6, 7 };
+            List<Person> listPerson = new List<Person>()
+            {
+                new Person(),
+                new Person(){Age = 12}
+
+            };
+            Dictionary<int, string> listDictionary = new Dictionary<int, string>()
+            {
+                {1,"123" },
+                {2,"fd" }
+            };
+            #endregion
+
+            #region 知识点四 匿名类型
+            //var 变量可以声明为自定义的匿名类型
+            var v = new { age = 10, money = 11, name = "小明" };
+            Console.WriteLine(v.age);
+            Console.WriteLine(v.name);
+            #endregion
+
+            #region 知识点五 可空类型
+            //1.值类型是不能赋值为 空的
+
+            //2.声明时 在值类型后面加? 可以赋值为空
+            int? c = null;//C已经为结构体
+            c = 6;
+            //3.判断是否为空
+            if (c.HasValue)
+            {
+                Console.WriteLine(c);
+                Console.WriteLine(c.Value);
+            }
+            //4.安全获取可空类型值
+            int? value = null;
+            //4.1 如果为空 默认返回值类型的默认值
+            Console.WriteLine(value.GetValueOrDefault());
+            //4.2也可以指定一个默认值
+            Console.WriteLine(value.GetValueOrDefault(100));
+            //Console.WriteLine(value);
+
+            object o = null;
+            //相当于一种语法糖 能够帮助我们自动去判断o是否为空
+            //如果是null 就不会执行tostring也不会报错
+            o?.ToString();
+            Action action = null;
+            action?.Invoke();
+            #endregion
+
+            #region 知识点六 空合并操作符
+            //空合并操作符 ??
+            //左边值 ?? 右边值
+            //如果左边值为 null 就返回右边值 否则返回左边值
+            //只要是可以为null的类型都能用
+            int? intV = null;
+            int intI = intV == null ? 100 : intV.Value;
+            int intInt = intV ?? 100;
+            Console.WriteLine(intI);
+            Console.WriteLine(intInt);
+            #endregion
+
+            #region 知识点七 内插字符串
+            //关键符号: $
+            //用$来构造字符串，让字符串可以拼接变量
+            string name = "周健";
+            Console.WriteLine($"好好学习，{name}");
+            #endregion
+
+            #region 知识点八 单句逻辑简略写法
+             //函数只有一句可以直接     =>  就行
+            #endregion
+        }
+    }
+}
