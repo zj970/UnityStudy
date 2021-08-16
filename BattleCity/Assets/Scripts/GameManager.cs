66fa1f55bb5962ea1a5b8d75e724f30e4df1dd94
zj970 <3060529292@qq.com>
2021-08-16 13:32:30 +0800

    坦克大战--布局--移动

---
+++ BattleCity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    //weight:22,height:18,enter (0,0)
+    public float x_length = 11f;
+    public float y_length = 7.5f;
+
+    public GameObject AirWall;
+    public GameObject Maps;
+
+    private void Start()
+    {
+        CreatAirWall();
+    }
+
+    //TODO:Creat a few AirWall on the Map
+    void CreatAirWall()
+    {
+        //Creat Up and Down AirWalls
+        for (float i = -x_length; i <= x_length; i++)
+        {
+            //first Down(-11,-8.5,0)/Up(11,8.5f,0)
+            Vector3 posDown = new Vector3(i, -8.5f, 0);
+            Vector3 posUp = new Vector3(i, 8.5f, 0);
+            if (AirWall != null)
+            {
+                GameObject airWallDown = Instantiate(AirWall, posDown, AirWall.transform.rotation);//instantiation this AirWaill
+                GameObject airWallUp = Instantiate(AirWall, posUp, AirWall.transform.rotation);
+                airWallDown.transform.SetParent(Maps.transform);//Sort out
+                airWallUp.transform.SetParent(Maps.transform);
+            }
+
+        }
+        //Creat Lift and Right AirWalls
+        for (float j = -y_length; j <= y_length; j++)
+        {
+            //first Lift(-11,-7.5,0)/Right(11,7.5f,0)
+            Vector3 posLift = new Vector3(11, j, 0);
+            Vector3 posRight = new Vector3(-11, j, 0);
+            if (AirWall != null)
+            {
+                GameObject airWallRight = Instantiate(AirWall, posRight, AirWall.transform.rotation);//instantiation this AirWaill
+                GameObject airWallLeft = Instantiate(AirWall, posLift, AirWall.transform.rotation);
+                airWallRight.transform.SetParent(Maps.transform);
+                airWallLeft.transform.SetParent(Maps.transform);
+            }
+
+        }
+    }
+
+
+}
---
+++ BattleCity/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float horizontal = 0f;
+    public float vertical = 0f;
+    public float speed = 2f;
+
+    public bool isVerticalAvailable = false;
+    private Vector3 playerRotation = Vector3.zero;
+
+    //TODO:The keyboard controls the player's movement;
+    private void Update()
+    {
+        //Input.GetAxis("");//判断我们键盘输入，左右方向键 -1，0，1,也就是可以达到我们控制的目的
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.UpArrow)|| Input.GetKeyDown(KeyCode.W)|| Input.GetKeyDown(KeyCode.S))
+        {
+            isVerticalAvailable = true;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.A)|| Input.GetKeyDown(KeyCode.D))
+        {
+            isVerticalAvailable = false;
+        }
+        if (isVerticalAvailable)
+        {
+            if (vertical > 0)
+            {
+                playerRotation = new Vector3(0, 0, 180);
+            }
+            if (vertical < 0)
+            {
+                playerRotation = Vector3.zero;
+            }
+        }
+
+        if (!isVerticalAvailable)
+        {
+            if (horizontal > 0)
+            {
+                playerRotation = new Vector3(0, 0, 90);
+
+            }
+            if (horizontal < 0)
+            {
+                playerRotation = new Vector3(0, 0, -90);
+            }
+        }
+        this.transform.rotation = Quaternion.Euler(playerRotation);//重置游戏对象的Rotation
+
+        if (vertical != 0 || horizontal != 0)
+        {
+            this.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+    }
+}
